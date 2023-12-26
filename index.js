7199d67e6058c7bed03c283a511244d92372876f
nirtendra <nirtendrayadav12@gmail.com>
2023-12-26 20:49:56 +1100

    creates variable for arithmatic operations

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random()*5+1);
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3%num4 ;
+
+let max = Math.max(1,2,15,20)
